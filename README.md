a52d74dcf39f81e7ff84e458fca8a5785b8ebd59
Jamie Wells <jamiewells528@gmail.com>
2025-10-05 16:47:15 +0100

    update readme

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Solstice Matching Engine
 
-A high-performance limit order book written in modern C++ (C++23), designed for experimentation with order flow, benchmarking, and execution algorithms.
+A high-performance limit order book written in C++23, designed for experimentation with order flow, benchmarking, and execution algorithms.
 
 ## Benchmark (v0.1)
 
 **Config:**
-- Orders: 1,000,000
-- Tickers: 3
-- Price Range: 10–20
-- Quantity Range: 1–10
+- Orders: 100,000
+- Tickers: 10
+- Price Range: $8–$10 (to 2 d.p)
+- Quantity Range: 1–20
 
 **Result:**
-- Matched: 486,730
-- Time Taken: 14,041 ms
-- Throughput: ~71,200 orders/sec
+- Matched: 49,788
+- Time Taken: 1,225 ms
+- Throughput: ~81,600 orders/sec
 
 ## Build
 
--- src/config/config.h
+++ src/config/config.h
@@ -14,21 +14,21 @@ struct Config
    public:
     static std::expected<Config, std::string> initConfig();
 
-    LogLevel d_logLevel = LogLevel::INFO;
+    LogLevel d_logLevel = LogLevel::DEBUG;
 
     // number of orders to generate
-    int d_ordersToGenerate = 100000;
+    int d_ordersToGenerate = 1;
 
     // number of unique tickers that orders can come in for
-    int d_tkrPoolCount = 3;
+    int d_tkrPoolCount = 10;
 
     // min and max quanties of each order
     int d_minQnty = 1;
-    int d_maxQnty = 10;
+    int d_maxQnty = 20;
 
     // mix and max price of each order
-    double d_minPrice = 10.0;
-    double d_maxPrice = 20.0;
+    double d_minPrice = 8.0;
+    double d_maxPrice = 10.0;
 
    private:
     Config();
